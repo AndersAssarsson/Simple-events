647ab150ac28286ee880f218cdd0059d56738a0a
John Doe <123@mmm.dk>
2016-11-10 11:45:11 +0100

    besked1

---
+++ Simple-events/Simple-events/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Simple_events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+                Observable observerble = new Observable();
+                Observer observer = new Observer();
+                observerble.SomethingHappened += observer.HandleEvent;
+
+                observerble.DoSomething();
+            }
+    }
+}
